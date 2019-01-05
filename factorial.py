f4d0d040f18acd0b37e3bbb9191e17b2a06d1d36
Igor Fernandes <fernandesigor09@gmail.com>
2019-01-05 18:45:47 -0200

    Implementing repeat loop to calculate factorial

--- factorial.py
+++ factorial.py
@@ -4,6 +4,12 @@ check_number = value == 0 or value == 1
 
 if (check_number):
     result = 1
-    print('The result {}! is {}'.format(value, result))
+    print('The result : {}! is {}'.format(value, result))
 
+else:
+    factorial = 1
+    for count in range(1, value + 1):
 
+        factorial *= count
+        
+    print('The result : {}! is {}'.format(value, factorial))
